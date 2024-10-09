2d86e08e4776dcf7684feb5505c6abd576e2cf63
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-10-09 11:48:58 -0400

    feat: implement services related to book operations

--- client/src/app/core/services/book.service.ts
+++ client/src/app/core/services/book.service.ts
@@ -16,7 +16,19 @@ export class BookService {
     return this.http.get<Book[]>(this.apiUrl);
   }
 
+  getBookById(bookId: number): Observable<Book> {
+    return this.http.get<Book>(`${this.apiUrl}/${bookId}`);
+  }
+
   addBook(book: Book): Observable<Book> {
     return this.http.post<Book>(this.apiUrl, book);
-  }  
+  }
+
+  updateBook(book: Book): Observable<Book> {
+    return this.http.put<Book>(`${this.apiUrl}/${book.id}`, book);
+  }
+
+  deleteBook(bookId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${bookId}`);
+  }
 }
