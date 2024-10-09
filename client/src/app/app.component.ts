657cfa8b6b5d2196a5c11c44093673ea6cbdb9e1
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-10-09 09:38:15 -0400

    feat: implement book form componenent

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from "./shared/layout/sidenav/sidenav.component";
-
 @Component({
   selector: 'app-root',
   standalone: true,
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { BookListComponent } from './features/book/book-list/book-list.component';
+import { BookFormComponent } from './features/book/book-form/book-form.component';
 
 export const routes: Routes = [
     { path: 'books', component: BookListComponent },
+    { path: 'add-book', component: BookFormComponent },
     { path: '', redirectTo: '/books', pathMatch: 'full' }
 ];
