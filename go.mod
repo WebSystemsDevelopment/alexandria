9a8f6b96cb1712062d0e092dab021f1ee5c75d47
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-10-09 12:31:16 -0400

    feat: enable middleware for frontend communication

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/spec v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
@@ -28,6 +29,7 @@ require (
 	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.25.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- rest-api/cmd/api/main.go
+++ rest-api/cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/WebSystemsDevelopment/alexandria/rest-api/docs" // This is for Swagger documentation
 	"github.com/WebSystemsDevelopment/alexandria/rest-api/internal/routes"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
@@ -14,6 +15,12 @@ import (
 // @BasePath /
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"}, // Change "*" to your specific frontend URL in production
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.OPTIONS},
+	}))
+
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	routes.RegisterRoutes(e)
