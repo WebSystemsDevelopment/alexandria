8005e5f18117cdd49cdc9f104e6719bb87845641
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-10-09 10:24:11 -0400

    feat: add sidenav in app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from "./shared/layout/sidenav/sidenav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SidenavComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
