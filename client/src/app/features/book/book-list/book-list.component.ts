5aa5870d1bd2d25e3f2332fad59993a631d5bc63
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-10-09 11:22:27 -0400

    feat: enhance actions and styles in book list

--- client/src/app/features/book/book-list/book-list.component.ts
+++ client/src/app/features/book/book-list/book-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 import { Book } from '../../../core/models/book.model';
 import { BookService } from '../../../core/services/book.service';
 
@@ -9,14 +10,15 @@ import { BookService } from '../../../core/services/book.service';
   standalone: true,
   imports: [ 
     HttpClientModule,
-    MatTableModule 
+    MatTableModule,
+    MatIconModule
   ],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css',
   providers: [BookService]
 })
 export class BookListComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'title', 'author', 'isbn'];
+  displayedColumns: string[] = ['id', 'title', 'author', 'isbn', 'actions'];
   books: Book[] = [];
 
   constructor(private bookService: BookService) {}
@@ -25,5 +27,17 @@ export class BookListComponent implements OnInit {
     this.bookService.getBooks().subscribe(books => {
       this.books = books;
     });
+  }
+
+  onAddBook() {
+    console.log('Add Book');
+  }
+
+  onEditBook(bookId: number) {
+    console.log('Edit Book', bookId);
+  }
+
+  onDeleteBook(bookId: number) {
+    console.log('Delete Book', bookId);
   }
 }
